39738e5f2b3ba700693c4f4331550ac026862b24
EC2 Default User <ec2-user@ip-172-31-47-206.ap-southeast-1.compute.internal>
2018-09-30 08:35:57 +0000

    first commit

---
+++ db/migrate/20180930051811_add_n_ickname_to_users.rb
@@ -0,0 +1,5 @@
+class AddNIcknameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :nickname, :string
+  end
+end
---
+++ db/migrate/20180930061243_remove_name_fromtweets.rb
@@ -0,0 +1,4 @@
+class RemoveNameFromtweets < ActiveRecord::Migration
+  def change
+  end
+end
